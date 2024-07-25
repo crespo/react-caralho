d859b897c3ef9ec703be4267b044bb859784b2b2
raul crespo <raulxcrespo@gmail.com>
2024-07-25 16:53:07 -0300

    conditional rendering

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,32 @@
 import { IAdmin } from "../../models/Admin"
 
-let admin: IAdmin = {name: "rauuuuuuul"};
+let admin: IAdmin = {name: "Raul"};
 
 function Presentation() {
     return (
         <h2>My name is {admin.name}!</h2>
     )
 }
+
+function UnknownUser() {
+    return (
+        <h2>{"How you doing, fellow user? =)"}</h2>
+    )
+}
+
 export default function Home() {
+    let content;
+    let isRaul = admin.name === "Raul";
+
+    if (isRaul) {
+        content = <Presentation /> 
+    } else {
+        content = <UnknownUser />
+    }
     return (
         <>
             <h1>Hello World!</h1>
-            <Presentation />
+            {content}
         </>
     )
 }
